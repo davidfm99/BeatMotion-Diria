3a9e7d6435a4eecf0dd0f12bcb1713b31d317c95
andreyjpg <andreyjpg61@gmail.com>
2025-09-29 17:10:55 -0600

    fixes (#39)

---
+++ BeatMotion-Diria/app/private/_layout.tsx
@@ -0,0 +1,27 @@
+import { getAuth, set, ref } from "firebase/database";
+import { View } from "react-native/Libraries/Components/View/View";
+import { Stack } from "expo-router";
+
+export default function TabLayout() {
+  //   const auth = getAuth();
+  //   const user = auth.currentUser;
+
+  //   onAuthStateChanged(auth, (user) => {
+  //     if (user) {
+  //       // User is signed in, see docs for a list of available properties
+  //       // https://firebase.google.com/docs/reference/js/firebase.User
+  //       const uid = user.uid;
+  //       console.log("Usuario logeado con uid:", uid);
+  //       // ...
+  //     } else {
+  //       // User is signed out
+  //       console.log("Usuario no logeado");
+  //     }
+  //   });
+
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+    </Stack>
+  );
+}
