41f86b14507d9e5244c68e2cc11dbd614acf9123
andreyjpg <andreyjpg61@gmail.com>
2025-11-11 19:13:11 -0600

    fix

--- BeatMotion-Diria/app/private/user/course/[courseId].tsx
+++ BeatMotion-Diria/app/private/user/course/[courseId].tsx
@@ -3,7 +3,7 @@ import YouTubeVideoPlayer from "@/components/YoutubeVideoPlayer";
 import { getEnrollmentColor, statusTranslations } from "@/constants/helpers";
 import { useClassesByCourseId } from "@/hooks/classes/useClassesByCourseId";
 import { useCourseDetail } from "@/hooks/courses/useCourseDetail";
-import { useActiveUser } from "@/hooks/UseActiveUser";
+import { useActiveUser } from "@/hooks/user/UseActiveUser";
 import { Ionicons } from "@expo/vector-icons";
 import { useLocalSearchParams } from "expo-router";
 import { useState } from "react";
