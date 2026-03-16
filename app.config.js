41719b9c6621da59e5e211a2e3056a8a4696ab48
andreyjpg <andreyjpg61@gmail.com>
2026-03-15 21:51:02 -0600

    fixes and android build

--- app.config.js
+++ app.config.js
@@ -1,89 +1,92 @@
-import "dotenv/config";
+export default ({ config }) => {
+  const ENV = process.env.EXPO_PUBLIC_APP_ENV ?? "development";
 
-const ENV = process.env.EXPO_PUBLIC_APP_ENV ?? "preview";
+  const isProd = ENV === "production";
 
-const isProd = ENV === "production";
+  const appName = true ? "BeatMotion" : "BeatMotion QA";
 
-const appName = isProd ? "BeatMotion" : "BeatMotion QA";
+  const androidPackage = false
+    ? "com.andreydev.beatmotion"
+    : "com.andreydev.beatmotion.qa";
 
-const androidPackage = isProd
-  ? "com.andreydev.beatmotion"
-  : "com.andreydev.beatmotion.qa";
+  const iosBundleIdentifier = false
+    ? "com.andreydev.beatmotion"
+    : "com.andreydev.beatmotion.qa";
 
-const iosBundleIdentifier = isProd
-  ? "com.andreydev.beatmotion"
-  : "com.andreydev.beatmotion.qa";
-
-export default {
-  expo: {
-    name: appName,
-    slug: "BeatMotion-Diria",
-    version: "1.0.0",
-    orientation: "portrait",
-    icon: "./assets/images/BeatMotionLogo.png",
-    scheme: "beatmotiondiria",
-    userInterfaceStyle: "automatic",
-    newArchEnabled: true,
-    owner: "beatmotion",
-    ios: {
-      bundleIdentifier: iosBundleIdentifier,
-      supportsTablet: true,
-      infoPlist: {
-        ITSAppUsesNonExemptEncryption: false,
+  return {
+    expo: {
+      name: appName,
+      slug: "BeatMotion-Diria",
+      version: "1.0.0",
+      orientation: "portrait",
+      icon: "./assets/images/BeatMotionLogo.png",
+      scheme: isProd ? "beatmotiondiria" : "beatmotiondiria-qa",
+      userInterfaceStyle: "automatic",
+      newArchEnabled: true,
+      owner: "beatmotion",
+      ios: {
+        bundleIdentifier: iosBundleIdentifier,
+        supportsTablet: true,
+        infoPlist: {
+          ITSAppUsesNonExemptEncryption: false,
+        },
       },
-    },
-    android: {
-      package: androidPackage,
-      adaptiveIcon: {
-        foregroundImage: "./assets/images/BeatMotionLogo.png",
-        backgroundColor: "#000000",
+      android: {
+        package: androidPackage,
+        googleServicesFile: isProd
+        ? "./google-services-production.json"
+        : "./google-services-preview.json",
+        adaptiveIcon: {
+          foregroundImage: "./assets/images/BeatMotionLogo.png",
+          backgroundColor: "#000000",
+        },
+        edgeToEdgeEnabled: true,
+        predictiveBackGestureEnabled: false,
       },
-      edgeToEdgeEnabled: true,
-      predictiveBackGestureEnabled: false,
-    },
-    web: {
-      output: "static",
-      favicon: "./assets/images/favicon.png",
-    },
-    plugins: [
-      "expo-router",
-      [
-        "expo-splash-screen",
-        {
-          image: "./assets/images/splash-icon.png",
-          imageWidth: 200,
-          resizeMode: "contain",
-          backgroundColor: "#ffffff",
-          dark: {
-            backgroundColor: "#000000",
+      web: {
+        output: "static",
+        favicon: "./assets/images/favicon.png",
+      },
+      plugins: [
+        "expo-router",
+        [
+          "expo-splash-screen",
+          {
+            image: "./assets/images/splash-icon.png",
+            imageWidth: 200,
+            resizeMode: "contain",
+            backgroundColor: "#ffffff",
+            dark: {
+              backgroundColor: "#000000",
+            },
           },
-        },
+        ],
+        "expo-web-browser",
+        [
+          "expo-notifications",
+          {
+            icon: "./assets/images/BeatMotionLogo.png",
+            color: "#000000",
+          },
+        ],
+        [
+          "@sentry/react-native/expo",
+          {
+            url: "https://sentry.io/",
+            project: "react-native",
+            organization: "beatmotion",
+          },
+        ],
       ],
-      "expo-web-browser",
-      [
-        "expo-notifications",
-        {
-          icon: "./assets/images/BeatMotionLogo.png",
-          color: "#000000",
+      experiments: {
+        typedRoutes: true,
+        reactCompiler: false,
+      },
+      extra: {
+        eas: {
+          projectId: "e88f7e8e-f8d4-4a7f-bd33-507f1afc4cf2",
         },
-      ],
-      [
-        "@sentry/react-native/expo",
-        {
-          url: "https://sentry.io/",
-          project: "react-native",
-          organization: "beatmotion",
-        },
-      ],
-    ],
-    experiments: {
-      typedRoutes: true,
-      reactCompiler: false,
-    },
-    extra: {
-      eas: {
-        projectId: "e88f7e8e-f8d4-4a7f-bd33-507f1afc4cf2",
       },
     },
-  },
+  };
 };
