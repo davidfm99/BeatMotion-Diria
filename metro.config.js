a273f51e37691f3ac73cf3da12056e3e0087f968
andreyjpg <andreyjpg61@gmail.com>
2026-06-20 17:20:37 -0600

    add privacy and update packages

--- metro.config.js
+++ metro.config.js
@@ -5,4 +5,12 @@ const {
 
 const config = getSentryExpoConfig(__dirname);
 
+// Firebase v12+ uses private class fields (#field) which Metro skips by default
+config.transformer = {
+  ...config.transformer,
+  transformIgnorePatterns: [
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?(/.*)?|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|nativewind|firebase|@firebase)',
+  ],
+};
+
 module.exports = withNativeWind(config, { input: "./global.css" });
